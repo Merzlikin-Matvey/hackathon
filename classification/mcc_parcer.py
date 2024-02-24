e3ed95ad81196d162102955393df2af25744cfbc
DukeTime <duke.of.times@gmail.com>
2024-02-24 12:22:03 +0300

    suda

--- classification/mcc_parcer.py
+++ classification/mcc_parcer.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup as BS
 request = requests.get(f'https://mcc-codes.ru/search/?q={'KFC'}')
 bs = BS(request.content, 'html.parser')
 
-
+data = list(bs.findAll("a"))
+for i in data:
+    if i.text.isdigit(): print(i.text) 
