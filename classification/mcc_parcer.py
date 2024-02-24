ca9de73708dc690890db8b9e41c1597b68942c1f
DukeTime <duke.of.times@gmail.com>
2024-02-24 14:35:53 +0300

    mcc 4

--- classification/mcc_parcer.py
+++ classification/mcc_parcer.py
@@ -1,26 +1,36 @@
-#Перед запуском: pip install requests pandas beautifulsoup4 openpyxl
+# Перед запуском: pip install requests pandas beautifulsoup4 openpyxl
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 
-request = requests.get(f'https://mcc-codes.ru/search/?q={'KFC'}')
-bs = BS(request.content, 'html.parser')
-
+file = open("classification/mcc_codes.txt", "w", encoding="utf-8")
+# Запись всех комапаний из исторических данных
 try:
     all_organisations = pd.read_excel('../data/clear_data.xlsx')['merchant_name'].unique()
 except FileNotFoundError:
     all_organisations = pd.read_excel('data/clear_data.xlsx')['merchant_name'].unique()
 
+file.write(f'ВСЕГО КОМПАНИЙ {len(all_organisations)}\n\n')
+progress = 0
+for organisation in all_organisations:
+    request = requests.get(f'https://mcc-codes.ru/search/?q={'+'.join(organisation.split())}') # Открываем поиск кодов для кампании
+    bs = BS(request.content, 'html.parser')
+    data = list(bs.findAll("a")) # Ищем все коды
+    file.write('\n' + organisation + ':')
+    org_types = []
+    for i in data:
+        if i.text.isdigit(): 
+            if len(str(int(i.text))) == 4:
+                # Запоминаем тип фирмы
+                org_types.append('\n    ' + f'({i.text})' + i['title'])
+    # Запись в файл
+    if org_types == []: file.write('ДАННЫХ НЕ НАЙДЕННО')
+    for type in list(set(org_types)):
+        file.write(type)
+    progress += 1
+    print(f'PROGRESS {progress}/{len(all_organisations)}')
 
-print(all_organisations)
-data = list(bs.findAll("a"))
-for i in data:
+file.close()# Реплит норм тема
 
-    if i.text.isdigit(): 
-        print(i.text)
-        try:
-            print(i['title'])
-        except:
-            100
 
 
