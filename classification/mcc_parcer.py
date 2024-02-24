5990af7f620769e79ac6cfaedbf8d1b014a3a40e
Matvey Merzlikin <merzlikinmatvey01@gmail.com>
2024-02-24 12:51:54 +0300

    Фикс под VS code

--- classification/mcc_parcer.py
+++ classification/mcc_parcer.py
@@ -6,8 +6,10 @@ from bs4 import BeautifulSoup as BS
 request = requests.get(f'https://mcc-codes.ru/search/?q={'KFC'}')
 bs = BS(request.content, 'html.parser')
 
-all_organisations = pd.read_excel('../data/clear_data.xlsx')['merchant_name'].unique()
-
+try:
+    all_organisations = pd.read_excel('../data/clear_data.xlsx')['merchant_name'].unique()
+except:
+    all_organisations = pd.read_excel('data/clear_data.xlsx')['merchant_name'].unique()
 print(all_organisations)
 data = list(bs.findAll("a"))
 for i in data:
